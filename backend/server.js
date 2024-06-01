75f156cb933b92225bfd3603d5cb65645a5b2832
tejaprf <bollatejaprf@gmail.com>
2024-06-01 17:07:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ const app = express();
 
 dotenv.config();
 app.use(cors());
+app.use(cors({
+  origin: 'https://quizapp-fend.onrender.com'
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
